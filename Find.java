69ace04a65dd33f63ebcac2b2560660672bce61a
skhan176 <43834676+skhan176@users.noreply.github.com>
2019-06-14 19:42:55 -0400

    Find.java

---
+++ Find.java
@@ -0,0 +1,42 @@
+public  class Find{
+	public static void main(String[] args) {
+		
+		System.out.println(sum());
+		
+	}
+	
+  public static  boolean sum(){
+	  int a[]={9,4,5,2,-1};
+	  int t=0;
+	  int s=-1;
+	  for(int i=0;i<a.length;i++){
+		if ( a[i]==t || a[i]==s){
+			return true;
+			}
+	  }
+	return false;
+	  }
+	}
+		
+				
+
+	  
+
+				  
+	   
+		  
+			  
+			  
+				  
+		  
+		  
+		
+		 
+    
+		  
+		
+		 
+    
+	  
+	  
+	  
